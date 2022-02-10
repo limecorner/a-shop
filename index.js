85bc1ac8661fc3dc57f7643e1208312003b1315b
Limecorner <iversonwang40@gmail.com>
2022-02-10 22:55:18 +0800

    feat: click button to control forms

---
+++ index.js
@@ -0,0 +1,51 @@
+const form = document.querySelector('#a-form')
+const formParts = form.querySelectorAll('.part')
+const stepControl = document.querySelector('.stepper-panel')
+const steps = stepControl.querySelectorAll('.step')
+const btnControl = document.querySelector('#btn-control')
+const nextBtn = btnControl.querySelector('.btn-primary')
+const prevBtn = btnControl.querySelector('.btn-outline')
+let step = 0
+
+function handleBtnControlClicked(e) {
+  e.preventDefault()
+  const nowStep = steps[step]
+  if (e.target.matches('.btn-primary') && e.target.children[0].innerText === '下一步') {
+    const nextStep = steps[step + 1]
+    nowStep.classList.remove('active')
+    nowStep.classList.add('checked')
+    nextStep.classList.add('active')
+    formParts[step].classList.toggle('d-none')
+    formParts[step + 1].classList.toggle('d-none')
+    step += 1
+  } else if (e.target.matches('.btn-outline')) {
+    const prevStep = steps[step - 1]
+    nowStep.classList.remove('active')
+    prevStep.classList.remove('checked')
+    prevStep.classList.add('active')
+    formParts[step].classList.toggle('d-none')
+    formParts[step - 1].classList.toggle('d-none')
+    step -= 1
+  }
+  setBtnState()
+}
+
+function setBtnState() {
+  if (step === 0) {
+    prevBtn.classList.remove('btn-mobile-half')
+    prevBtn.classList.add('d-none')
+    nextBtn.classList.remove('btn-mobile-half')
+  }
+  if (step === 1) {
+    prevBtn.classList.remove('d-none')
+    prevBtn.classList.add('btn-mobile-half')
+    nextBtn.classList.add('btn-mobile-half')
+  }
+  if (step === 2) {
+    nextBtn.children[0].innerText = '確認下單'
+  } else {
+    nextBtn.children[0].innerText = '下一步'
+  }
+}
+
+btnControl.addEventListener('click', handleBtnControlClicked)
